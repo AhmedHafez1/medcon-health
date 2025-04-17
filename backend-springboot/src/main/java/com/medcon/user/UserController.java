3c686b18c034910d7c00d53e2ab06f137616c1e5
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2025-04-17 20:58:03 +0200

    GET /api/users

--- backend-springboot/src/main/java/com/medcon/user/UserController.java
+++ backend-springboot/src/main/java/com/medcon/user/UserController.java
@@ -3,12 +3,15 @@ package com.medcon.user;
 import com.medcon.auth.dto.UserDto;
 import com.medcon.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/api/users")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
@@ -16,5 +19,11 @@ public class UserController {
     @GetMapping("/me")
     public UserDto getCurrentUser() {
         return userService.getCurrentUser();
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping
+    public List<UserDto> getAllUsers() {
+        return userService.getAllUsers();
     }
 }
--- backend-springboot/src/main/java/com/medcon/user/service/UserService.java
+++ backend-springboot/src/main/java/com/medcon/user/service/UserService.java
@@ -5,15 +5,23 @@ import com.medcon.exception.UnAuthorizedException;
 import com.medcon.user.mapper.UserMapper;
 import com.medcon.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
-@RequiredArgsConstructor
+import java.util.List;
+
 @Service
 public class UserService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+
+    @Autowired
+    public UserService(UserRepository userRepository, UserMapper userMapper) {
+        this.userRepository = userRepository;
+        this.userMapper = userMapper;
+    }
 
     public UserDto getCurrentUser() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -27,5 +35,9 @@ public class UserService {
                 .orElseThrow(() -> new UnAuthorizedException("User not found!"));
 
         return userMapper.toDto(user);
+    }
+
+    public List<UserDto> getAllUsers() {
+        return userRepository.findAll().stream().map(userMapper::toDto).toList();
     }
 }
