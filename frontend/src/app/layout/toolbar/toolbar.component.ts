d7ba99e394856eba19d7c89b7c8a2fa867d91c3d
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2025-04-29 07:33:54 +0300

    feat: enhance toolbar layout and user menu, update sidebar styles, and modify global theming

--- frontend/src/app/layout/toolbar/toolbar.component.ts
+++ frontend/src/app/layout/toolbar/toolbar.component.ts
@@ -4,6 +4,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatDivider } from '@angular/material/divider';
 @Component({
   selector: 'app-toolbar',
   imports: [
@@ -12,6 +13,7 @@ import { MatMenuModule } from '@angular/material/menu';
     MatIconModule,
     MatButtonModule,
     MatMenuModule,
+    MatDivider,
   ],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.scss',
