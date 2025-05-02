a96f1b9d08c4f5f17151f13535298ea01c2890d6
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2025-05-02 23:23:33 +0300

    feat: refactor layout structure, enhance sidebar styles, and update material theming

--- frontend/src/app/layout/layout.component.ts
+++ frontend/src/app/layout/layout.component.ts
@@ -3,11 +3,20 @@ import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { RouterOutlet } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [CommonModule, SidebarComponent, ToolbarComponent, RouterOutlet],
+  imports: [
+    CommonModule,
+    MatSidenavModule,
+    MatButton,
+    SidebarComponent,
+    ToolbarComponent,
+    RouterOutlet,
+  ],
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss'],
 })
